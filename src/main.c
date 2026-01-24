c7c4d1775a9c6028f5960f960ebb252a8c2b5b7d
Carlos Bolívar <cebemind@hotmail.com>
2026-01-24 00:07:14 -0400

    Wrong commants handling

--- src/main.c
+++ src/main.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  // TODO: Uncomment the code below to pass the first stage
   printf("$ ");
+
+  char input[100];
+  fgets(input, sizeof(input), stdin);
+
+  input[strlen(input) - 1] = '\0'; // Remove newline character
+  printf("%s: command not found\r\n", input);
 
   return 0;
 }
