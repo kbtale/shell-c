55c4f3d652043e4f13fa9e132fb7ee320dc49ce0
Carlos Bolívar <cebemind@hotmail.com>
2026-01-24 01:21:40 -0400

    Add exit command handling

--- src/main.c
+++ src/main.c
@@ -16,11 +16,11 @@ int main(int argc, char *argv[]) {
     if (strncmp(input, "exit", 4) == 0) {
       if (input[4] == '\n' || input[4] == '\0' || input[4] == ' ') {
         break;
+      }
     }
 
     input[strlen(input) - 1] = '\0'; // Remove newline character
     printf("%s: command not found\r\n", input);
   }
   return 0;
-  
 }
