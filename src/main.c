37b55bfa3c6fc50644f02ce4f25f8e7fa3e029f2
Carlos Bolívar <cebemind@hotmail.com>
2026-01-24 01:24:21 -0400

    Add the echo command

--- src/main.c
+++ src/main.c
@@ -19,6 +19,11 @@ int main(int argc, char *argv[]) {
       }
     }
 
+    if (strncmp(input, "echo ", 5) == 0) {
+      printf("%s\r\n", input + 5);
+      continue;
+    }
+    
     input[strlen(input) - 1] = '\0'; // Remove newline character
     printf("%s: command not found\r\n", input);
   }
