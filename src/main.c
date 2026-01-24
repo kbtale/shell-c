0fa062104020de3b2346a82a53f888475950f83b
Carlos Bolívar <cebemind@hotmail.com>
2026-01-24 01:10:17 -0400

    REPL Loop

--- src/main.c
+++ src/main.c
@@ -6,13 +6,16 @@ int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  printf("$ ");
-
-  char input[100];
-  fgets(input, sizeof(input), stdin);
-
-  input[strlen(input) - 1] = '\0'; // Remove newline character
-  printf("%s: command not found\r\n", input);
-
+  while (1)
+  {
+    printf("$ ");
+  
+    char input[100];
+    fgets(input, sizeof(input), stdin);
+  
+    input[strlen(input) - 1] = '\0'; // Remove newline character
+    printf("%s: command not found\r\n", input);
+  }
   return 0;
+  
 }
