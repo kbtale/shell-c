6bc28e14038eb162e4dfb2902216ffa450452ca3
Carlos Bolívar <cebemind@hotmail.com>
2026-01-24 01:40:12 -0400

    Add the echo command

--- src/main.c
+++ src/main.c
@@ -5,13 +5,14 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
-
+  
   while (1)
   {
     printf("$ ");
-  
+    
     char input[100];
     fgets(input, sizeof(input), stdin);
+    input[strlen(input) - 1] = '\0'; // Remove newline character
     
     if (strncmp(input, "exit", 4) == 0) {
       if (input[4] == '\n' || input[4] == '\0' || input[4] == ' ') {
@@ -32,7 +33,6 @@ int main(int argc, char *argv[]) {
       continue;
     }
 
-    input[strlen(input) - 1] = '\0'; // Remove newline character
     printf("%s: command not found\r\n", input);
   }
   return 0;
