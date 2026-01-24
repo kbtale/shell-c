88a90bdecea68194876cd3bd524480dcebd4218f
Carlos Bolívar <cebemind@hotmail.com>
2026-01-24 18:27:55 -0400

    adding the type command

--- src/main.c
+++ src/main.c
@@ -5,7 +5,9 @@
 int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
-  
+  const char *builtins[] = {"echo", "exit", "type"};
+  size_t num_builtins = sizeof(builtins) / sizeof(builtins[0]);
+
   while (1)
   {
     printf("$ ");
@@ -30,6 +32,30 @@ int main(int argc, char *argv[]) {
       if (input[4] == ' ')
         printf("%s\r\n", input + 5);
       
+      continue;
+    }
+
+    if (strncmp(input, "type", 4) == 0) {
+      
+      if (input[4] == '\0') {
+        printf("type: usage: type COMMAND\r\n");
+        continue;
+      }
+
+      if (input[4] == ' ') {
+        char *command = input + 5;
+        int found = 0;
+        for (int i = 0; i < num_builtins; i++) {
+          if (strcmp(command, builtins[i]) == 0) {
+            printf("%s is a shell builtin\r\n", command);
+            found = 1;
+            break;
+          }
+        }
+        if (!found) {
+          printf("%s: not found\r\n", command);
+        }
+      }
       continue;
     }
 
